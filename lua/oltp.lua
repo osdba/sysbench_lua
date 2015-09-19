f72c766e5e333dc726b63264ecda3020e1440bb7
cheng.tang <cheng.tang@woqutech.com>
2015-09-19 11:37:20 +0800

    oltp.lua, support pg

--- lua/oltp.lua
+++ lua/oltp.lua
@@ -76,8 +76,12 @@ function event(thread_id)
 ###########-###########-###########-###########-###########-###########-###########-###########-###########-###########]])
    pad_val = sb_rand_str([[
 ###########-###########-###########-###########-###########]])
-
-   rs = db_query("INSERT INTO " .. table_name ..  " (id, k, c, pad) VALUES " .. string.format("(%d, %d, '%s', '%s')",i, sb_rand(1, oltp_table_size) , c_val, pad_val))
+   
+   if db_driver == "pgsql" then
+       rs = db_query("INSERT INTO " .. table_name ..  " (k, c, pad) VALUES " .. string.format("(%d, '%s', '%s')",sb_rand(1, oltp_table_size) , c_val, pad_val))
+   else
+       rs = db_query("INSERT INTO " .. table_name ..  " (id, k, c, pad) VALUES " .. string.format("(%d, %d, '%s', '%s')",i, sb_rand(1, oltp_table_size) , c_val, pad_val))
+   end
 
    end -- oltp_read_only
 
